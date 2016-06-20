92afe87071c3bba5d6df076b89acebdb31cafe30
Luís Goulart <luis_goulart_316@hotmail.com>
2016-06-20 23:47:03 +0100

    table2

--- src/app/home/entity-list.component.ts
+++ src/app/home/entity-list.component.ts
@@ -1,40 +1,57 @@
 import { Component, OnInit } from '@angular/core';
 import {DatePipe} from "@angular/common";
-import {HTTP_PROVIDERS, Http} from "@angular/http";
 import {DataTableDirectives} from 'angular2-datatable/datatable';
+//import {HTTP_PROVIDERS, Http, Response} from "@angular/http";
 import * as _ from 'lodash';
+import { TOOLTIP_DIRECTIVES } from 'ng2-bootstrap/ng2-bootstrap';
+import {EntityListService} from './entity-list.service';
+
 
 @Component({
     selector: 'entity-list',
     template: require('./entity-list.component.html'),
-    providers: [HTTP_PROVIDERS],
-    directives: [DataTableDirectives],
+    providers: [EntityListService],
+    directives: [DataTableDirectives, TOOLTIP_DIRECTIVES],
     pipes: [DatePipe]
 })
 
 export class EntityListComponent implements OnInit{
 
-    private data;
+    private data: any;
+    dataLength: number;
 
-    constructor(private http:Http) {
-       http.get("app/home/table-data.json")
-            .subscribe((data)=> {
-                setTimeout(()=> {
-                    this.data = data.json();
-                }, 1000);
-            });
+    constructor(private entityListService: EntityListService) {
     }
-    
+
     public ngOnInit() {
-        console.log(this.data);
+      this.getData();
+      console.log("ngOninit() " + this.dataLength);
+    }
+
+    public getData() {
+      this.entityListService.getData().subscribe(
+        (data) => {
+          //console.log(data.length),
+          this.getLength(data),
+          this.data = data
+        },
+        error => console.log("Error HTTP GET Service"),
+        () => console.log("Job Done Get !")
+      );
+    }
+
+    private getLength(param: any) {
+      console.log("getLenght() :" + param.length);
+      dataLength => this.dataLength = param.length;
     }
 
     private sortByWordLength = (a:any) => {
         return a.fieldA.length;
     }
-    
+
     public removeItem(item: any) {
       this.data = _.filter(this.data, (elem)=>elem!=item);
       console.log("Remove: ", item.fieldA);
     }
+
 }
---
+++ src/app/home/entity-list.service.ts
@@ -0,0 +1,32 @@
+import {Injectable} from 'angular2/core';
+import {HTTP_PROVIDERS, Http, Response} from "@angular/http";
+import { Observable }     from 'rxjs/Observable';
+
+
+@Injectable()
+export class EntityListService {
+
+  constructor(private http:Http) {}
+
+  public getData(){
+    return this.http.get("app/home/table-data.json")
+    .map(this.extractData);
+  }
+
+  private extractData(res: Response) {
+  let body = res.json();
+  return body.data || { };
+}
+
+  private handleError (error: any) {
+    // In a real world app, we might use a remote logging infrastructure
+    // We'd also dig deeper into the error to get a better message
+    let errMsg = (error.message) ? error.message :
+      error.status ? `${error.status} - ${error.statusText}` : 'Server error';
+    console.error(errMsg); // log to console instead
+    return Observable.throw(errMsg);
+  }
+
+
+
+}
